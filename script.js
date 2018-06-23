a14f547dc26eef74c5cfec1eab0a8bec23b2afff
Saqib Ali <sa248848@gmail.com>
2018-06-24 01:38:01 +0500

    js-table

---
+++ script.js
@@ -0,0 +1,9 @@
+	var num = parseInt(prompt("enter any number"));
+
+	for (var i=1; i<=10;i++){
+
+		document.write(" <br> " , num , " * " , i , " = " +  num*i);
+
+	}
+	
+	document.write(" <br>outside block " , " <br> " , " the value of count is " , count);
